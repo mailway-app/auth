f7c5c99027385334a7af00fcf229eb9ab1b7cd50
Sven Sauleau <sven@cloudflare.com>
2021-01-24 19:13:48 +0000

    add configurable ports

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mailway-app/auth
+
+go 1.13
+
+require github.com/mailway-app/config v0.0.0-20210124184150-485238e4123e
--- server.go
+++ server.go
@@ -3,11 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	mailwayConfig "github.com/mailway-app/config"
 )
 
+var config *mailwayConfig.Config
+
 func main() {
+	var err error
+	config, err = mailwayConfig.Read()
+	if err != nil {
+		panic(err)
+	}
+
 	http.HandleFunc("/", AuthServer)
-	if err := http.ListenAndServe("127.0.0.1:9000", nil); err != nil {
+	addr := fmt.Sprintf("127.0.0.1:%d", config.PortAuth)
+	if err := http.ListenAndServe(addr, nil); err != nil {
 		panic(err)
 	}
 }
@@ -16,7 +27,7 @@ func AuthServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("auth request")
 	w.Header().Set("Auth-Status", "OK")
 	w.Header().Set("Auth-Server", "127.0.0.1")
-	w.Header().Set("Auth-Port", "2500")
+	w.Header().Set("Auth-Port", fmt.Sprintf("%d", config.PortForwarding))
 	w.WriteHeader(200)
 	w.Write([]byte(""))
 }
