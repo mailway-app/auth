991ebe93dcbcbc9ab6b2fae219c9e0827a6ea417
Sven Sauleau <sven@cloudflare.com>
2021-02-14 17:10:13 +0000

    config update

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/mailway-app/auth
 go 1.13
 
 require (
-	github.com/mailway-app/config v0.0.0-20210213003639-e697b1dd86fb
+	github.com/mailway-app/config v0.0.0-20210214145136-8a381a1ce901
 	github.com/sirupsen/logrus v1.7.0
-	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
 )
--- server.go
+++ server.go
@@ -4,34 +4,28 @@ import (
 	"fmt"
 	"net/http"
 
-	mailwayConfig "github.com/mailway-app/config"
+	"github.com/mailway-app/config"
 
 	log "github.com/sirupsen/logrus"
 )
 
-var config *mailwayConfig.Config
-
 func main() {
-	var err error
-	config, err = mailwayConfig.Read()
-	if err != nil {
-		log.Fatal(err)
+	if err := config.Init(); err != nil {
+		log.Fatalf("failed to init config: %s", err)
 	}
-	log.SetLevel(config.GetLogLevel())
-	log.SetFormatter(config.GetLogFormat())
 
 	http.HandleFunc("/", AuthServer)
-	addr := fmt.Sprintf("127.0.0.1:%d", config.PortAuth)
+	addr := fmt.Sprintf("127.0.0.1:%d", config.CurrConfig.PortAuth)
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func AuthServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("auth request")
+	log.Debugf("auth request")
 	w.Header().Set("Auth-Status", "OK")
 	w.Header().Set("Auth-Server", "127.0.0.1")
-	w.Header().Set("Auth-Port", fmt.Sprintf("%d", config.PortForwarding))
+	w.Header().Set("Auth-Port", fmt.Sprintf("%d", config.CurrConfig.PortForwarding))
 	w.WriteHeader(200)
 	w.Write([]byte(""))
 }
